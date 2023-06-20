52115defbb7944cbe46651ea376f1d0ce61b16a2
Juan Antonio <ja.rauldidiaquez@gmail.com>
2023-06-20 01:25:55 -0400

    prototipo final listo

--- myscript.js
+++ myscript.js
@@ -4,15 +4,33 @@ cutOffTime.setHours(12);
 cutOffTime.setMinutes(0);
 cutOffTime.setSeconds(0);
 
-// Calcular tiempo restante
-let horasFaltantes = Math.abs(cutOffTime.getHours() - now.getHours());
-let minutosFaltantes = Math.abs(cutOffTime.getMinutes() - now.getMinutes());
-let segundosFaltantes = Math.abs(cutOffTime.getSeconds() - now.getSeconds());
-
+//Revisa si es día de semana o no.
 function checkWeek(now) {
     const diaSemana = now.getDay();
     return diaSemana >= 1 && diaSemana <= 5;
 }
+
+// Calcular tiempo restante separado horas, minutos, segundos
+let horasFaltantes = cutOffTime.getHours() - now.getHours();
+let minutosFaltantes = cutOffTime.getMinutes() - now.getMinutes();
+let segundosFaltantes = cutOffTime.getSeconds() - now.getSeconds();
+
+// Calcular la diferencia de tiempo
+let timeDifference = cutOffTime.getTime() - now.getTime();
+timeDifference = Math.max(timeDifference, 0);
+
+// Función para actualizar la cuenta regresiva cada segundo
+function updateCountdown() {
+    const now = new Date();
+    let timeDifference = cutOffTime.getTime() - now.getTime();
+    timeDifference = Math.max(timeDifference, 0);
+    horasFaltantes = Math.floor(timeDifference / (1000 * 60 * 60));
+    minutosFaltantes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    segundosFaltantes = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    
+    mensajeHoras();
+}
+
 
 function mensajeHoras() {
     document.getElementById("delivery-message").innerHTML = "Si compras ahora tu pedido llegará hoy.<br> Compra dentro de <span class='remaining-time'>" + horasFaltantes + " hora, " + minutosFaltantes + " minutos, y " + segundosFaltantes + " segundos.</span><br><span class='santiago'>Sólo en la provincia de Santiago</span>";
@@ -24,6 +42,7 @@ function deliveryMessage() {
         if (now.getTime() < cutOffTime.getTime()) {
             console.log('Tu pedido llega hoy');
             mensajeHoras();
+            setInterval(updateCountdown, 1000);
         } else {
             console.log('Tu pedido llega mañana');
         }
@@ -35,8 +54,3 @@ function deliveryMessage() {
 }
 
 deliveryMessage();
-
-// Calcular tiempo restante
-const remainingHours = cutOffTime.getHours() - now.getHours();
-const remainingMinutes = cutOffTime.getMinutes() - now.getMinutes();
-const remainingSeconds = cutOffTime.getSeconds() - now.getSeconds();
