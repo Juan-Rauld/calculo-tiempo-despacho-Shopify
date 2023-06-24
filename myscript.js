36112d7a1b4f73908aaddd97557c55b61204e06a
Juan Antonio <ja.rauldidiaquez@gmail.com>
2023-06-24 14:25:10 -0400

    se agregan console.log para ir revisando qué día es.

--- myscript.js
+++ myscript.js
@@ -25,14 +25,19 @@ function deliveryMessage() {
     if (checkWeek(now)) {
         if (now.getDay() === 5 && now.getTime() > cutOffTime.getTime()) {
             document.getElementById("delivery-message").innerHTML = 'Tu pedido llega el Lunes porque hoy es Viernes después del mediodía';
+            console.log('Es viernes, pero se pasó la hora de corte.' + ' Hoy es el día nº ' + now.getDay())
+
         } else if (now.getDay() === 1 && now.getTime() < cutOffTime.getTime()) {
             updateCountdown(cutOffTime);
             setInterval(() => updateCountdown(cutOffTime), 1000);
             document.getElementById("delivery-message").innerHTML = "Si compras ahora tu pedido llegará hoy.<br> Compra dentro de <span class='remaining-time'>" + Math.abs(horasFaltantes) + " horas, " + Math.abs(minutosFaltantes) + " minutos, y " + Math.abs(segundosFaltantes) + " segundos.</span><br><span class='santiago'>Sólo en la provincia de Santiago</span>";
+            console.log('Sí se cumplen las condiciones para el despacho el mismo día. Dia Laboral (entre Lunes y Viernes, antes de las 12' + ' Hoy es el día nº ' + now.getDay())
         } else {
+            console.log('Es día Laboral y ya pasó la hora de corte' + ' Hoy es el día nº ' + now.getDay())
             document.getElementById("delivery-message").innerHTML = 'Tu pedido llega mañana';
         }
     } else {
+        console.log('Es fin de semana, la regla genera que se despache el próximo Lunes.' + ' Hoy es el día nº ' + now.getDay())
         document.getElementById("delivery-message").innerHTML = 'Tu pedido llega el Lunes';
     }
 }
