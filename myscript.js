df89f0ca61f21b57ddd7e5dd642ba3d617809f66
Juan Antonio <ja.rauldidiaquez@gmail.com>
2023-07-08 23:23:45 -0400

    se agregó consulta API de feriados y 2 reglas para (si hoy dia feriado, se entrega dia siguiente habil; si hoy +12:00 en dia laboral, se entrega siguiente dia habil ya que maána es feriado

--- myscript.js
+++ myscript.js
@@ -1,9 +1,40 @@
+async function obtenerFeriados() {
+    try {
+        const response = await fetch('https://apis.digital.gob.cl/fl/feriados');
+        if (!response.ok) {
+            console.error('Error en la respuesta de la API', response);
+            return [];
+        }
+        const data = await response.json();
+        const esteAno = (new Date()).getFullYear();
+        const proximoAno = esteAno + 1;
+        const feriados = data.filter(feriado => feriado.fecha.startsWith(esteAno) || feriado.fecha.startsWith(proximoAno));
+        return feriados;
+    } catch (error) {
+        console.error('Error al obtener feriados:', error);
+        return [];
+    }
+}
+
+
+function esFeriado(date, feriados) {
+    const fecha = date.toISOString().split('T')[0];
+    return feriados.some(feriado => feriado.fecha === fecha);
+}
+
+function siguienteDiaHabil(date, feriados) {
+    const nextDate = new Date(date);
+    do {
+        nextDate.setDate(nextDate.getDate() + 1);
+    } while (nextDate.getDay() === 0 || nextDate.getDay() === 6 || esFeriado(nextDate, feriados));
+    return nextDate;
+}
+
 function checkWeek(now) {
     const diaSemana = now.getDay();
     return diaSemana >= 1 && diaSemana <= 5;
 }
 
-// Función para actualizar la cuenta regresiva cada segundo
 function updateCountdown(cutOffTime) {
     const now = new Date();
     let timeDifference = cutOffTime.getTime() - now.getTime();
@@ -15,22 +46,23 @@ function updateCountdown(cutOffTime) {
     document.getElementById("delivery-message").innerHTML = "Si compras ahora tu pedido llegará hoy.<br> Compra dentro de <span class='remaining-time'>" + Math.abs(horasFaltantes) + " horas, " + Math.abs(minutosFaltantes) + " minutos, y " + Math.abs(segundosFaltantes) + " segundos.</span><br><span class='santiago'>Sólo en la provincia de Santiago</span>";
 }
 
-function deliveryMessage() {
-    const now = new Date();
-    const cutOffTime = new Date();
-    cutOffTime.setHours(12);
-    cutOffTime.setMinutes(0);
-    cutOffTime.setSeconds(0);
+async function deliveryMessage() {
+    const feriados = await obtenerFeriados();
 
-    if (checkWeek(now)) {
+    const now = new Date();
+    const cutOffTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0);
+
+    if (esFeriado(now, feriados)) {
+        document.getElementById("delivery-message").innerHTML = 'Entrega al día siguiente hábil';
+    } else if (checkWeek(now) && now.getTime() > cutOffTime.getTime() && esFeriado(siguienteDiaHabil(now, feriados), feriados)) {
+        document.getElementById("delivery-message").innerHTML = 'El despacho es el próximo día laboral hábil';
+    } else if (checkWeek(now)) {
         if (now.getDay() === 5 && now.getTime() > cutOffTime.getTime()) {
             document.getElementById("delivery-message").innerHTML = 'Tu pedido llega el Lunes porque hoy es Viernes después del mediodía';
             console.log('Es viernes, pero se pasó la hora de corte.' + ' Hoy es el día nº ' + now.getDay())
-
-        } else if (now.getDay() === 1 && now.getTime() < cutOffTime.getTime()) {
+        } else if ([1, 2, 3, 4].includes(now.getDay()) && now.getTime() < cutOffTime.getTime()) {
             updateCountdown(cutOffTime);
             setInterval(() => updateCountdown(cutOffTime), 1000);
-            document.getElementById("delivery-message").innerHTML = "Si compras ahora tu pedido llegará hoy.<br> Compra dentro de <span class='remaining-time'>" + Math.abs(horasFaltantes) + " horas, " + Math.abs(minutosFaltantes) + " minutos, y " + Math.abs(segundosFaltantes) + " segundos.</span><br><span class='santiago'>Sólo en la provincia de Santiago</span>";
             console.log('Sí se cumplen las condiciones para el despacho el mismo día. Dia Laboral (entre Lunes y Viernes, antes de las 12' + ' Hoy es el día nº ' + now.getDay())
         } else {
             console.log('Es día Laboral y ya pasó la hora de corte' + ' Hoy es el día nº ' + now.getDay())
